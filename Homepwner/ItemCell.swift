281decede4c0f195368c122fafc7872244b7e181
Luke Markham <luke@Sarahs-MacBook-Pro.local>
2017-08-20 20:01:15 +0100

    Moving between computers

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Luke Markham on 20/08/2017.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
--- Item.swift
+++ Item.swift
@@ -1,4 +1,4 @@
-//
+            //
 //  Item.swift
 //  Homepwner
 //
